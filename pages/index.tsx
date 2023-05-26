9a17c72a7e7fb7ae483b0c93813d344fb0478009
Erik Nguyen <46208016+erikphinguyen@users.noreply.github.com>
2023-05-26 15:15:35 -0700

    added gradient background container sc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 
 
 export default function Home() {
@@ -12,8 +13,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-      </main>
+      {/* Background */}
+      <GradientBackgroundCon>
+
+      </GradientBackgroundCon>
     </>
   )
 }
