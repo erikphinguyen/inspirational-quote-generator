2be038c913e1415cb6e8114cf17b9e2e4d2f4cbb
Erik Nguyen <erikphinguyen@gmail.com>
2023-05-26 22:42:27 -0700

    amplify configured and connected, starting graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 
 // Components
-import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, RedSpan, GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, RedSpan, GradientBackgroundCon, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorTitle, QuoteGeneratorSubTitle, GenerateQuoteButton, GenerateQuoteButtonText } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 
 // Assets
 import Clouds1 from '../assets/cloud-and-thunder.png'
@@ -14,7 +14,7 @@ import Clouds2 from '../assets/cloudy-weather.png'
 
 export default function Home() {
 
-  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null >(0);
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
 
   return (
     <>
@@ -27,6 +27,31 @@ export default function Home() {
       {/* Background */}
       <GradientBackgroundCon>
 
+
+        {/* Quote Generator Modal Pop-Up */}
+        {/* <QuoteGeneratorModal /> */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Inspirational Quote Generator
+            </QuoteGeneratorTitle>
+
+            <QuoteGeneratorSubTitle>
+              Looking for a splash of inspiration? Generate a quote card with a random inspirational quote provided by <FooterLink href="https://zenquotes.io/" target="_blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>.
+            </QuoteGeneratorSubTitle>
+
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+                Generate Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* Background Images */}
         <BackgroundImage1
           src={Clouds1}
           alt="cloudybackground1"
