8404f0cabe5dbf23d0e9a1e81cf697e6ca4c9199
Erik Nguyen <erikphinguyen@gmail.com>
2023-05-26 19:02:17 -0700

    finished basic set up -- starting react

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -4,20 +4,58 @@ import Image from 'next/image';
 
 
 export const GradientBackgroundCon = styled.div`
-    background: linear-gradient()to right, #000046, #1CB5E0;
-    background-size: 400% 400%;
-    animation: gradient 6s ease infinite;
-    height: 100vh;
-    width: 100vw;
-    @keyframes gradient {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
+  background: linear-gradient(to right, #000046, #1cb5e0);
+  background-size: 400% 400%;
+  animation: gradient 6s ease infinite;
+  height: 100vh;
+  width: 100vw;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
     }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
+
+export const BackgroundImage1 = styled(Image)`
+    position: relative;
+    z-index: 1;
+    margin-left: -7%;
+    margin-top: -10%;
+    height: auto;
+    width: auto;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+    position: fixed;
+    z-index: 1;
+    margin-left: 19%;
+    margin-top: 24%;
+    height: auto;
+    width: 60%;
+`;
+
+export const FooterCon = styled.div`
+width: 100vw;
+height: 50px;
+text-align: center;
+font-family: 'Source Code Pro', monospace;
+font-size: 15px;
+position: absolute;
+bottom: 0;
+color: white;
+z-index: 999999;
+`;
+
+export const FooterLink = styled(Link)`
+    color: white;
+`;
+
+export const RedSpan = styled.span`
+    color: red;
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
+import React, {useState} from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Components
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, RedSpan, GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Assets
+import Clouds1 from '../assets/cloud-and-thunder.png'
+import Clouds2 from '../assets/cloudy-weather.png'
 
 
 export default function Home() {
+
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null >(0);
+
   return (
     <>
       <Head>
@@ -16,6 +27,24 @@ export default function Home() {
       {/* Background */}
       <GradientBackgroundCon>
 
+        <BackgroundImage1
+          src={Clouds1}
+          alt="cloudybackground1"
+        />
+
+        <BackgroundImage2
+          src={Clouds2}
+          alt="cloudybackground1"
+        />
+
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with <RedSpan>❤️</RedSpan> by <FooterLink href="https://www.linkedin.com/in/erikphinguyen/" target="_blank" rel="noopener noreferrer">Erik Nguyen
+            </FooterLink>
+          </>
+        </FooterCon>
       </GradientBackgroundCon>
     </>
   )
